db690dee8b179561565eb0253c3eddb89f46381e
Aaron Katzin <katzin.aaron@gmail.com>
2022-10-12 21:22:37 +0300

    Working new game button

--- app/src/main/java/com/hit/game012/Win.java
+++ app/src/main/java/com/hit/game012/Win.java
@@ -1,8 +1,10 @@
-package com.hit.game012.gameplay;
+package com.hit.game012;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Win extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Win extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win);
+    }
+
+    public void newGame(View view){
+        Intent intent = new Intent(this, ChooseBoardSize.class);
+        intent.putExtra("isDailyGame", false);
+        startActivity(intent);
     }
 }
