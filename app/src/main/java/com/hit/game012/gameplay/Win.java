909eb210277a333e22f8abf8f977d774beae69dc
Aaron Katzin <katzin.aaron@gmail.com>
2022-10-12 20:58:11 +0300

    empty win activity

---
+++ app/src/main/java/com/hit/game012/gameplay/Win.java
@@ -0,0 +1,14 @@
+package com.hit.game012.gameplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Win extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_win);
+    }
+}
