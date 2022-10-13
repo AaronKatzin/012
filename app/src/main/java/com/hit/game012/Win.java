580d49460c94f8b910195c787875dfc9a8104605
Aaron Katzin <katzin.aaron@gmail.com>
2022-10-13 19:00:48 +0300

    stash  tablelayout

--- app/src/main/java/com/hit/game012/Win.java
+++ app/src/main/java/com/hit/game012/Win.java
@@ -5,7 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class Win extends AppCompatActivity {
 
@@ -14,17 +20,51 @@ public class Win extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win);
 
-        long gameTime = (long) getIntent().getExtras().get("gameTime");
-        int hintCounter = (int) getIntent().getExtras().get("hintCounter");
+        long gameTime;
+        int hintCounter;
+
+        if(false) {
+            gameTime = (long) getIntent().getExtras().get("gameTime");
+            hintCounter = (int) getIntent().getExtras().get("hintCounter");
+        }
+        else{
+            gameTime = 100;
+            hintCounter = 1;
+        }
 
         long score = gameTime + hintCounter;
 
         TextView scoreText = findViewById(R.id.scoreText);
         scoreText.setText("gameTime: " + gameTime + "\n hintCounter: " + hintCounter+ "\n score: " + score);
+
+        createTable(5,2);
     }
 
     public void newGame(View view){
         Intent intent = new Intent(this, ChooseBoardSize.class);
         startActivity(intent);
+    }
+
+    private void createTable(int rows, int columns){
+        TableLayout table = new TableLayout(this);
+        table.setLayoutParams(new TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        table.setShrinkAllColumns(true);
+        table.setStretchAllColumns(true);
+        
+        Random random = new Random();
+
+        for (int i=0; i < rows; i++) {
+            TableRow row = new TableRow(Win.this);
+            for (int j=0; j < columns; j++) {
+                int value = random.nextInt(100) + 1;
+                TextView tv = new TextView(Win.this);
+                tv.setText(String.valueOf(value));
+                row.addView(tv);
+            }
+            table.addView(row);
+        }
+
+        ScrollView tableScroller = findViewById(R.id.multiplayerHighScoreTable);
+        tableScroller.addView(table);
     }
 }
