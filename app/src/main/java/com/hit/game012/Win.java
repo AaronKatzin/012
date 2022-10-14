34c613ecffc274f6d487dc0806626873176abdf9
Aaron Katzin <katzin.aaron@gmail.com>
2022-10-14 09:59:26 +0300

    cleanup

--- app/src/main/java/com/hit/game012/Win.java
+++ app/src/main/java/com/hit/game012/Win.java
@@ -75,39 +75,6 @@ public class Win extends AppCompatActivity {
         MyListAdapter adapter=new MyListAdapter(this, maintitle, subtitle,imgid);
         list=(ListView)findViewById(R.id.list);
         list.setAdapter(adapter);
-        list.setItemsCanFocus(false);
-
-//
-//        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
-//                // TODO Auto-generated method stub
-//                if(position == 0) {
-//                    //code specific to first list item
-//                    Toast.makeText(getApplicationContext(),"Place Your First Option Code",Toast.LENGTH_SHORT).show();
-//                }
-//
-//                else if(position == 1) {
-//                    //code specific to 2nd list item
-//                    Toast.makeText(getApplicationContext(),"Place Your Second Option Code",Toast.LENGTH_SHORT).show();
-//                }
-//
-//                else if(position == 2) {
-//
-//                    Toast.makeText(getApplicationContext(),"Place Your Third Option Code",Toast.LENGTH_SHORT).show();
-//                }
-//                else if(position == 3) {
-//
-//                    Toast.makeText(getApplicationContext(),"Place Your Forth Option Code",Toast.LENGTH_SHORT).show();
-//                }
-//                else if(position == 4) {
-//
-//                    Toast.makeText(getApplicationContext(),"Place Your Fifth Option Code",Toast.LENGTH_SHORT).show();
-//                }
-//
-//            }
-//        });
     }
 
     public void newGame(View view){
