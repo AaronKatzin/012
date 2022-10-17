baabb0aaa477b38b6a1186add76acd2a2cc58200
Aaron Katzin <katzin.aaron@gmail.com>
2022-10-17 15:40:14 +0300

    copy entire robohash folder from https://github.com/neuhalje/android-robohash/tree/master/src/main/java/name/neuhalfen/projects/android/robohash

---
+++ app/src/main/java/com/hit/game012/ui/robohash/RoboHash.java
@@ -0,0 +1,89 @@
+package com.hit.game012.ui.robohash;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.LruCache;
+import com.hit.game012.ui.robohash.buckets.VariableSizeHashing;
+import com.hit.game012.ui.robohash.handle.Handle;
+import com.hit.game012.ui.robohash.handle.HandleFactory;
+import com.hit.game012.ui.robohash.paths.Configuration;
+import com.hit.game012.ui.robohash.paths.Set1Configuration;
+import com.hit.game012.ui.robohash.repository.ImageRepository;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class RoboHash {
+
+    private final static HandleFactory handleFactory = new HandleFactory();
+
+    private final Configuration configuration = new Set1Configuration();
+    private final ImageRepository repository;
+    private final VariableSizeHashing hashing = new VariableSizeHashing(configuration.getBucketSizes());
+
+
+    // Optional
+    private LruCache<String, Bitmap> memoryCache;
+
+    public RoboHash(Context context) {
+        this.repository = new ImageRepository(context.getAssets());
+    }
+
+    // //    untested
+    // public Handle calculateHandleFromBinary(byte[] binary) {
+    //     byte[] data = hashing.createBuckets(new BigInteger(binary));
+    //     return handleFactory.calculateHandle(data);
+    // }
+
+
+    /**
+     * @param memoryCache use the passed memory cache (can be null for no cache)
+     */
+    public void useCache(LruCache<String, Bitmap> memoryCache) {
+        this.memoryCache = memoryCache;
+    }
+
+    public Handle calculateHandleFromUUID(UUID uuid) {
+        byte[] data = hashing.createBuckets(uuid);
+        return handleFactory.calculateHandle(data);
+    }
+
+    /**
+     * This can be VERY slow (~15ms on a Nexus 5). Consider #useCache.
+     *
+     * @param handle which robot to retrieve
+     * @return image (300x300) that identifies the handle
+     * @throws IOException Unexpected, because this would mean the build is broken (FIXME: Use unchecked Exception)
+     */
+    public Bitmap imageForHandle(Handle handle) throws IOException {
+        if (null != memoryCache) {
+            Bitmap cached = memoryCache.get(handle.toString());
+            if (null != cached) return cached;
+        }
+
+        byte[] bucketValues = handle.bucketValues();
+        String[] paths = configuration.convertToFacetParts(bucketValues);
+
+        int sampleSize = 1;
+
+        Bitmap buffer = repository.createBuffer(configuration.width(), configuration.height());
+        Bitmap target = buffer.copy(Bitmap.Config.ARGB_8888, true);
+
+        Canvas merged = new Canvas(target);
+        Paint paint = new Paint(0);
+
+        // The first image is not added as copy form the buffer
+        for (int i = 0; i < paths.length; i++) {
+            merged.drawBitmap(repository.getInto(buffer, paths[i], sampleSize), 0, 0, paint);
+        }
+        repository.returnBuffer(buffer);
+
+        if (null != memoryCache) {
+            memoryCache.put(handle.toString(),target);
+        }
+        return target;
+    }
+
+}
---
+++ app/src/main/java/com/hit/game012/ui/robohash/repository/ImageRepository.java
@@ -0,0 +1,71 @@
+package com.hit.game012.ui.robohash.repository;
+
+
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ImageRepository {
+    private final AssetManager assets;
+    private Bitmap buffer = null;
+
+    public ImageRepository(AssetManager assets) {
+        this.assets = assets;
+    }
+
+
+    public Bitmap get(String path, int sampleSize) throws IOException {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = sampleSize;
+        options.inMutable = false;
+
+        InputStream inputStream = null;
+        try {
+            inputStream = assets.open(path);
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream, null, options);
+            return bitmap;
+        } finally {
+            if (null != inputStream) {
+                inputStream.close();
+            }
+        }
+    }
+
+    public Bitmap createBuffer(int width, int height) throws IOException {
+        if (buffer != null) {
+            return buffer;
+        }
+
+         buffer = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888); // this creates a MUTABLE bitmap
+
+        return buffer;
+
+    }
+
+    public Bitmap getInto(Bitmap target, String path, int sampleSize) throws IOException {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = sampleSize;
+        options.inBitmap = target;
+        options.inMutable = false;
+
+        InputStream inputStream = null;
+        try {
+            inputStream = assets.open(path);
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream, null, options);
+            return bitmap;
+        } finally {
+            if (null != inputStream) {
+                inputStream.close();
+            }
+        }
+    }
+
+
+    public void returnBuffer(Bitmap buffer) {
+            buffer.eraseColor(Color.TRANSPARENT);
+    }
+}
